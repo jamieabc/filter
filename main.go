bcc87eead32fcd08e923e5f206fa64203ffd43e1
Aaron Chang <xesuschang@gmail.com>
2019-08-18 16:51:27 +0800

    Initial commit to pipe stdin to stdout

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	//"bufio"
+	"fmt"
+	"io"
+	"os"
+
+	// "syscall"
+	"time"
+)
+
+const (
+	initialCheckTimeSecond = 200 * time.Millisecond
+	checkTimeSecond        = 5 * time.Second
+)
+
+func main() {
+	info, err := os.Stdin.Stat()
+	if nil != err {
+		panic(err)
+	}
+
+	if !isCharDevice(info.Mode()) {
+		fmt.Println("The command is intended to work with pipes.")
+		return
+	}
+
+	timer := time.After(initialCheckTimeSecond)
+
+	for {
+		select {
+		case <-timer:
+			for {
+				_, err := io.Copy(os.Stdout, os.Stdin)
+				if nil != err {
+					panic(err)
+				}
+			}
+			timer = time.After(checkTimeSecond)
+		}
+	}
+}
+
+func isCharDevice(mode os.FileMode) bool {
+	return 0 == mode&os.ModeCharDevice
+}
